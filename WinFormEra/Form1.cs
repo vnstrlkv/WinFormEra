a551f68ef27b365a16c5c39681d38a74e71a8b0c
vnstrlkv <vnstrlkv@gmail.com>
2018-01-08 21:44:12 +0700

    ad

--- WinFormEra/Form1.cs
+++ WinFormEra/Form1.cs
@@ -6,15 +6,24 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
+using System.Windows.Forms;
+using WDBFNS;
 namespace WinFormEra
 {
-    public partial class Form1 : Form
+public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            WDBF WDBF = new WDBF();
+            var dt = WDBF.GetAll("C:\\123\\personal.dbf");
+            dataGridView1.DataSource = dt;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
---
+++ WinFormEra/WDBF.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace WDBFNS
+{
+    class WDBF
+    {
+        // private OdbcConnection _connection = null;
+        private OleDbConnection _connection = null;
+        public DataTable Execute(string command)
+        {
+            DataTable dt = null;
+            if (_connection != null)
+            {
+                try
+                {
+                    _connection.Open();
+                    dt = new DataTable();
+                    //    System.Data.Odbc.OdbcCommand oCmd = _connection.CreateCommand();
+                    OleDbCommand oCmd = _connection.CreateCommand();
+                    oCmd.CommandText = command;
+                    dt.Load(oCmd.ExecuteReader());
+                    _connection.Close();
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+            return dt;
+        }
+        public DataTable GetAll(string dbpath)
+        {
+            return Execute("SELECT FIRST_LAST_NAME FROM " + dbpath);
+        }
+
+        public WDBF()
+        {
+            //  this._connection = new System.Data.Odbc.OdbcConnection();
+            this._connection = new OleDbConnection();
+            _connection.ConnectionString = @"Provider=vfpoledb;Data Source=C:\123\phoenix.dbc;Collating Sequence=machine; Exclusive=No";
+        }
+    }
+}
+
